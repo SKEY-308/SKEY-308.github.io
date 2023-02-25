01e8dc6a1ce84191d3a217a361ebc265ef315a22
Your Name <severus.key308@gmail.com>
2023-02-25 19:39:56 +0400

    25 Fev 19'39

--- script.js
+++ script.js
@@ -13,11 +13,20 @@ var typed = new typed(".typing", {
   loop: true,
 });
 
-// Aside
+// Side
 
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
   totalNavList = navList.length;
 for (let i = 0; i < totalNavList; i++) {
-  console.log(navList[i]);
+  //   console.log(navList[i]);
+  const a = navList[i].querySelector("a");
+  //   console.log(a);
+  a.addEventListener("click", function () {
+    // console.log(this);
+    for (let j = 0; j < totalNavList; j++) {
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+  });
 }
